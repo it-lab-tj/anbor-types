e408b7e347b6c57de3c4b60998edda5c80dc6ceb
Joniibek <jonibek.ortikov03@gmail.com>
2026-01-05 16:48:32 +0500

    some fixes

--- anbor_types/module/module/commands.py
+++ anbor_types/module/module/commands.py
@@ -1,4 +1,9 @@
+from anbor_types import ID_T, Command
 from anbor_types.module.module.dto import ModuleConnectDTO
 
 
 class ModuleConnectCommand(ModuleConnectDTO): ...
+
+
+class ModuleDeactivateCommand(Command):
+    default_module_id: ID_T
