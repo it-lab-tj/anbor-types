d1f74e16b67b2a68a3a7db29ecfbdb40a1a6f652
Joniibek <jonibek.ortikov03@gmail.com>
2026-01-06 17:08:51 +0500

    feat(category) some new kinds

--- anbor_types/catalog/category/dto.py
+++ anbor_types/catalog/category/dto.py
@@ -1,6 +1,26 @@
+from typing import Optional
+import msgspec
 from anbor_types import ID_T, BasePydanticModel
 
 
 class CharValueDTO(BasePydanticModel):
     characteristic_id: ID_T
     value_id: ID_T
+
+
+class ValuesListDTO(msgspec.Struct):
+    id: ID_T
+    name: str
+
+
+class CharacteristicDetailedDTO(msgspec.Struct):
+    id: ID_T
+    name: str
+    values: ValuesListDTO
+
+
+class CategoryDetailedDTO(msgspec.Struct):
+    id: ID_T
+    name: str
+    parent_id: Optional[ID_T]
+    characteristic: CharacteristicDetailedDTO
