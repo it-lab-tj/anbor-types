598453b2a886d049fe654fabe21e616cdaa382fe
m6d-dev <muhammadjon8757@gmail.com>
2026-04-20 14:18:40 +0500

    add sog confirm command

--- anbor_types/storage/sog/commands.py
+++ anbor_types/storage/sog/commands.py
@@ -6,3 +6,7 @@ from anbor_types.storage.so.commands import ApplicationStockOperationsCommand
 class SOGApplicationCommand(Command):
     stock_operations: List[ApplicationStockOperationsCommand]
     description: Optional[str] = None
+
+
+class SOGConfirmCommand(Command):
+    sog_id: int
--- anbor_types/storage/sog/results.py
+++ anbor_types/storage/sog/results.py
@@ -5,6 +5,7 @@ class SOGApplicationCreationResult(msgspec.Struct):
     sog_id: int
     vendor_code: str
 
+
 class SOGConfirmationResult(msgspec.Struct):
     sog_id: int
     vendor_code: str
