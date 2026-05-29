d4c65894552542b3d4824ebe6ab1659ddc573e51
m6d-dev <muhammadjon8757@gmail.com>
2026-05-29 15:53:06 +0500

    feat(subject) add dtos and commands

--- anbor_types/warehouse/business_document/subject/queries.py
+++ anbor_types/warehouse/business_document/subject/queries.py
@@ -1,7 +1,11 @@
-from anbor_types import ListQuery
+from anbor_types import ID_T, ListQuery, Query
 from anbor_types.warehouse.constants.enums import SubjectKindEnum
 
 
 class SubjectListQuery(ListQuery):
     kind: SubjectKindEnum
     status: int
+
+
+class SubjectDetailedQuery(Query):
+    id: ID_T
