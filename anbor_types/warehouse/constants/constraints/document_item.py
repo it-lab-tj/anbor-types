217f2b774f013c941b65e488caf7a98da3130693
Fakhriddin3040 <fahriddi3040@gmail.com>
2026-06-01 17:36:58 +0500

    feat(warehouse) Warehouse types updated.
    Signed-off-by: Fakhriddin3040 <fahriddi3040@gmail.com>

--- anbor_types/warehouse/constants/constraints/document_item.py
+++ anbor_types/warehouse/constants/constraints/document_item.py
@@ -1,12 +1,12 @@
 from decimal import Decimal
 
-COUNT_MAX = Decimal('1_000_000')
-COUNT_MIN = Decimal('0.01')
+COUNT_MAX = Decimal("1_000_000")
+COUNT_MIN = Decimal("0.01")
 
-PRICE_MAX = Decimal('100_000_000')
+PRICE_MAX = Decimal("100_000_000")
 PRICE_MIN = Decimal("0.0001")
 
-DISCOUNT_MAX = Decimal('100')
+DISCOUNT_MAX = Decimal("100")
 DISCOUNT_MIN = Decimal("0.1")
 
 CHAR_VALUES_MAX_COUNT = 15
