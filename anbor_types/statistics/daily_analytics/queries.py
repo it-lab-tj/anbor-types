5c189b05cda4a511fb7832b8808de114fc0170c0
m6d-dev <muhammadjon8757@gmail.com>
2026-06-15 16:34:02 +0500

    fix(catalog) fields DailyAnalyticListQuery

--- anbor_types/statistics/daily_analytics/queries.py
+++ anbor_types/statistics/daily_analytics/queries.py
@@ -1,5 +1,14 @@
-from anbor_types import ListQuery
+from datetime import date
+from decimal import Decimal
+from typing import Optional
+
+from anbor_types import Query
 
 
-class DailyAnalyticListQuery(ListQuery):
-    pass
+class DailyAnalyticListQuery(Query):
+    date_after: Optional[date] = None,
+    date_before: Optional[date] = None,
+    realisations_max: Optional[Decimal] = None,
+    realisations_min: Optional[Decimal] = None,
+    revenues_max: Optional[Decimal] = None,
+    revenues_min: Optional[Decimal] = None,
