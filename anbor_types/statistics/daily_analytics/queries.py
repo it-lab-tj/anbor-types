3517bf16026a00a3fa9eb453476882475c778678
m6d-dev <muhammadjon8757@gmail.com>
2026-06-16 09:57:53 +0500

    fix(catalog) fields DailyAnalyticListQuery

--- anbor_types/statistics/daily_analytics/queries.py
+++ anbor_types/statistics/daily_analytics/queries.py
@@ -1,14 +1,9 @@
-from datetime import date
-from decimal import Decimal
-from typing import Optional
+from typing import Any
+
+from pydantic import Field
 
 from anbor_types import Query
 
 
 class DailyAnalyticListQuery(Query):
-    date_after: Optional[date] = None,
-    date_before: Optional[date] = None,
-    realisations_max: Optional[Decimal] = None,
-    realisations_min: Optional[Decimal] = None,
-    revenues_max: Optional[Decimal] = None,
-    revenues_min: Optional[Decimal] = None,
+    filters: Any = Field(default_factory=tuple, init=False)
