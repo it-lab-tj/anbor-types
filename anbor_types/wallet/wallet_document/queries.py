fa6962d67b8c21c2642a4cca8189c56b46ae6836
Fakhriddin3040 <fahriddi3040@gmail.com>
2026-07-04 18:06:40 +0500

    feat(wallet) new types.
    Signed-off-by: Fakhriddin3040 <fahriddi3040@gmail.com>

--- anbor_types/wallet/wallet_document/queries.py
+++ anbor_types/wallet/wallet_document/queries.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from decimal import Decimal
 from typing import Annotated, Optional, Tuple
 
-from anbor_types import ID_T, ListQuery
+from anbor_types import ID_T, ListQuery, Query
 from anbor_types.api.annotated import ATSearch
 from anbor_types.api.types import OrderingAllowedFieldsT
 from anbor_types.common.constraints import DATETIME_MAX
@@ -11,6 +11,10 @@ from anbor_types.wallet.constants import WalletDocumentKindEnum
 from anbor_types.api.constants import PRICE_MAX, ID_MAX
 from anbor_types.utils.filter.types import FilterSpec
 from anbor_types.utils.filter.meta import FilterMeta
+
+
+class WalletDocumentDetailedQuery(Query):
+    id: ID_T
 
 
 class WalletTransferListQuery(ListQuery, OrderingQueryMixin, metaclass=FilterMeta):
