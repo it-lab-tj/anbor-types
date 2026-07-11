896e615e837724d257930b0e0cebea4c13c0b9aa
Joniibek <jonibek.ortikov03@gmail.com>
2026-07-11 16:45:14 +0500

    feat(catalog) category short dto added

--- anbor_types/catalog/category/queries.py
+++ anbor_types/catalog/category/queries.py
@@ -1,6 +1,13 @@
+from typing import Annotated
+
 from pydantic import Field
 
 from anbor_types import ID_T, ListQuery, Query
+from anbor_types.api.types import OrderingAllowedFieldsT
+from anbor_types.catalog.enums import CategoryKindEnum
+from anbor_types.utils.filter.meta import FilterMeta
+from anbor_types.utils.filter.types import FilterSpec
+from anbor_types.utils.mixins import OrderingQueryMixin
 
 
 class CategoryListQuery(ListQuery):
@@ -9,3 +16,17 @@ class CategoryListQuery(ListQuery):
 
 class CategoryCharacteristicsQuery(Query):
     category_id: ID_T
+
+
+class CategoryShortListQuery(ListQuery, OrderingQueryMixin, metaclass=FilterMeta):
+    _ordering_allowed_fields: OrderingAllowedFieldsT = {
+        "created_at",
+    }
+
+    kind: Annotated[
+        CategoryKindEnum,
+        FilterSpec.enum(
+            CategoryKindEnum,
+            description="**1** - Товар\n" "**2** - Услуга\n",
+        ),
+    ]
