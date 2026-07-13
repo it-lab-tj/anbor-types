0d681a4faa8f1ed194f8a9937080e5df6e010559
m6d-dev <muhammadjon8757@gmail.com>
2026-07-13 16:00:19 +0500

    feat(warehouse): add ordering, filter by balance in subject list

--- anbor_types/warehouse/business_document/subject/queries.py
+++ anbor_types/warehouse/business_document/subject/queries.py
@@ -1,21 +1,37 @@
-from typing import Annotated
+from decimal import Decimal
+from typing import Annotated, Tuple
 
 from anbor_types import ID_T, ListQuery, Query
-from anbor_types.api.filter_specs import AFStatus
+from anbor_types.api.constants import PRICE_MAX
 from anbor_types.api.queries import ShortListQuery
+from anbor_types.common.enums import StatusEnum
+from anbor_types.utils.filter.meta import FilterMeta
+from anbor_types.utils.mixins import OrderingQueryMixin
 from anbor_types.warehouse.constants.enums import SubjectKindEnum
 from anbor_types.utils.filter.types import FilterSpec
 
 
-class SubjectListQuery(ListQuery):
+class SubjectListQuery(ListQuery, OrderingQueryMixin, metaclass=FilterMeta):
+    _ordering_allowed_fields = {"created_at", "name"}
+
     kind: Annotated[
         SubjectKindEnum,
         FilterSpec.enum(
             SubjectKindEnum,
             description="**1** - Склад\n" "**2** - Клиент\n" "**3** - Исполнитель\n",
+            required=True,
         ),
     ]
-    status: AFStatus
+    status: Annotated[StatusEnum, FilterSpec.enum(StatusEnum, required=True)]
+
+    balance__rn: Annotated[
+        Tuple[Decimal, Decimal],
+        FilterSpec.numeric_range(
+            Decimal,
+            lte=PRICE_MAX,
+            gte=Decimal("0"),
+        ),
+    ]
 
 
 class SubjectShortListQuery(ShortListQuery):
