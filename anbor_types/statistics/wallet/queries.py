0c357a5c4fa050dc725cde41b4089925ba0a732e
Fakhriddin <fahriddin3040@gmail.com>
2026-09-02 15:28:45 +0500

    fix(wallet_doc) confirmed_at defaultl added.
    Signed-off-by: Fakhriddin <fahriddin3040@gmail.com>

--- anbor_types/statistics/wallet/queries.py
+++ anbor_types/statistics/wallet/queries.py
@@ -2,13 +2,21 @@ from typing import Annotated, Optional, Tuple
 
 from anbor_types.common.annotated import ATDatetime
 
-from anbor_types import Query
+from anbor_types import Query, ID_T
 from anbor_types.utils.filter.meta import FilterMeta
 from anbor_types.utils.filter.types import FilterSpec
+from anbor_types.api.constants import ID_MAX
 
 
 class WalletIncomeStatementQuery(Query, metaclass=FilterMeta):
     date__rn: Annotated[
         Tuple[Optional[ATDatetime], Optional[ATDatetime]],
         FilterSpec.datetime_range(both_required=True),
+    ]
+    project_id: Annotated[
+        Optional[ID_T],
+        FilterSpec.numeric(
+            int,
+            lte=ID_MAX,
+        ),
     ]
